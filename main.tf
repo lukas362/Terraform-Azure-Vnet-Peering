8d9698588b84e5a0daeec2e05c7a278862110a26
Lukas Svensson <lukassvensson761@gmail.com>
2025-12-20 19:55:04 +0100

    Terraform Template

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
     required_providers {
         azurerm = {
-            source = "hashicorp/azurerm"
-            version ="~> 3.0"
+            source  = "hashicorp/azurerm"
+            version = "~> 3.0"
         }
     }
 }
@@ -11,26 +11,32 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "jensen" {
-    name     = "jensen-resources"
-    location = "swedencentral"
+resource "azurerm_resource_group" "main" {
+    name     = var.resource_group_name
+    location = var.location
 
     tags = {
-        environment = "development"
-        owner       = "jensen"
+        environment = var.environment
+        owner       = var.owner
     }
 }
 
-resource "azurerm_virtual_network" "jensen" {
-    name                = "jensen-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.jensen.location
-    resource_group_name = azurerm_resource_group.jensen.name
+resource "azurerm_virtual_network" "main" {
+    name                = var.vnet_name
+    address_space       = var.vnet_address_space
+    location            = azurerm_resource_group.main.location
+    resource_group_name = azurerm_resource_group.main.name
+
+    tags = {
+        environment = var.environment
+        owner       = var.owner
+    }
 }
 
-resource "azurerm_subnet" "jensen" {
-    name                 = "jensen-subnet"
-    resource_group_name   = azurerm_resource_group.jensen.name
-    virtual_network_name = azurerm_virtual_network.jensen.name
-    address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_subnet" "main" {
+    name                 = var.subnet_name
+    resource_group_name  = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
+    address_prefixes     = var.subnet_address_prefix
 }
+
